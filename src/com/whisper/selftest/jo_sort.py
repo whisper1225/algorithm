2968f7ae7db3f95dff9a8df38c2c233a3bdf41f4
whisper <whisper1225@163.com>
2016-07-08 18:56:22 +0800

    not right

---
+++ src/com/whisper/selftest/jo_sort.py
@@ -0,0 +1,18 @@
+# coding:utf-8
+
+def jo_sort(array):
+    leng = len(array)
+    i = 0
+    j = leng - 1
+    while (i < j):
+        while ((array[i] % 2 ==1)  and i < leng):
+            i = i + 1
+        while  ((array[i] % 2 == 0) and j >= 0):
+            j = j - 1
+        if (i < j):
+            array[i], array[j] = array[j], array[i]
+    return array
+
+
+print(jo_sort([1, 2, 3, 4, 6]))
+
